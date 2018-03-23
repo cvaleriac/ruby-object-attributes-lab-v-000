c793cd7f71c2eabab71b1b6f01da078808c67079
valeria campilongo <cvaleriac@hotmail.it>
2018-03-23 17:48:37 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,18 +2,16 @@ class Dog
   def initialize(name)
     @name = name
   end
-def name
+  def name
   @name
   end
-  def initialize(breed)
-    @breed = breed
+end
+def initialize (breed)
+  @breed = breed
+end
+def breed
+  @breed
   end
-  def breed
-    @breed
-  end
-
 end
 fido = Dog.new("Fido")
-fido.name = "Fido"
 fido = Dog.new("Beagle")
-fido.breed = "Beagle"
